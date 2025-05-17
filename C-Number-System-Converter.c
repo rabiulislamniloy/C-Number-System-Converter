15c66ac45b7d3ab9d55612af40c7951d6ff50264
Rabiul Islam Niloy <154040740+rabiulislamniloy@users.noreply.github.com>
2025-05-17 20:42:59 +0600

    Add files via upload

---
+++ C-Number-System-Converter.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter a number ");
+    scanf("%d",&a);
+    printf("\nOctal Number is %o",a);
+    printf("\nHexadecimal Number is %X",a);
+    return 0;
+}
